755197dcc42689de4d57e9300183b35d5bae844d
Zongmin Lei <leizongmin@gmail.com>
2016-11-08 16:14:47 +0800

    getConnection() 支持 Promise

--- test/test_connection.js
+++ test/test_connection.js
@@ -26,7 +26,55 @@ describe('Connection', function () {
 
   it('connection ok', coroutine.wrap(function* () {
 
-    const conn = createConnection(getConnectionConfig());
+    const conn = createConnection({
+      connections: [ getConnectionConfig() ],
+    });
+    {
+      const ret = yield conn.query('SHOW TABLES');
+      console.log(ret);
+    }
+    {
+      const sql = conn.format('SELECT * FROM ::table WHERE id=:id', {
+        table: 'blog_contents',
+        id: 2,
+      });
+      console.log(sql);
+      const ret = yield conn.query(sql);
+      console.log(ret);
+    }
+    {
+      const sql = conn.format('SELECT * FROM ?? WHERE id=?', [ 'blog_contents', 2 ]);
+      console.log(sql);
+      const ret = yield conn.query(sql);
+      console.log(ret);
+    }
+    // {
+    //   const c = yield conn.getConnection();
+    //   console.log(c.escape(new Date()));
+    //   yield c.beginTransaction();
+    //   try {
+    //     const ret = yield c.query('INSERT INTO `blog_contents`(`id`,`content`) VALUES (1234, "456")');
+    //     console.log(ret);
+    //   } catch (err) {
+    //     console.log(err);
+    //     yield c.rollback();
+    //   }
+    //   try {
+    //     const ret = yield c.query('INSERT INTO `blog_contents`(`id`,`content`) VALUES (1234, "9999")');
+    //     console.log(ret);
+    //   } catch (err) {
+    //     console.log(err);
+    //     yield c.rollback();
+    //   }
+    //   try {
+    //     yield c.commit();
+    //   } catch (err) {
+    //     console.log(err);
+    //     yield c.rollback();
+    //   }
+    //   c.release();
+    // }
+    yield conn.close();
 
   }));
 
