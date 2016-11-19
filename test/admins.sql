64b8975453701b48dea5617b47b2745394f45f49
Zongmin Lei <leizongmin@gmail.com>
2016-11-19 10:27:43 +0800

    test manager

---
+++ test/admins.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `admins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `info` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- test/test_manager.js
+++ test/test_manager.js
@@ -9,21 +9,76 @@
 const expect = require('chai').expect;
 const coroutine = require('lei-coroutine');
 const { createManager } = require('../');
-const { getConnectionConfig, getCacheConfig } = require('./utils');
+const { getConnectionConfig, getCacheConfig, readTestFile } = require('./utils');
+const { randomString } = require('lei-utils');
 
 
 describe('Manager', function () {
 
-  it('ok', coroutine.wrap(function* () {
+  const prefix = randomString(10) + ':';
+  const manager = createManager(getCacheConfig({
+    connections: [ getConnectionConfig() ],
+    prefix,
+  }));
+  console.log(manager);
 
-    const manager = createManager(getCacheConfig({
-      connections: [ getConnectionConfig() ],
-    }));
+  before(coroutine.wrap(function* () {
+    const sql = yield readTestFile('admins.sql');
+    yield manager.connection.query('DROP TABLE IF EXISTS `admins`');
+    yield manager.connection.query(sql);
+  }));
 
-    console.log(manager);
-
+  after(coroutine.wrap(function* () {
     yield manager.close();
+  }));
 
+  it('registerModel', coroutine.wrap(function* () {
+    manager.registerModel('Admin', {
+      table: 'admins',
+      primary: 'id',
+      autoIncrement: true,
+      fields: {
+        id: true,
+        name: true,
+        email: true,
+        info: { type: 'json' },
+        created_at: true,
+      },
+    });
+  }));
+
+  it('hasModel', coroutine.wrap(function* () {
+    expect(manager.hasModel('Admin')).to.be.true;
+    expect(manager.hasModel('admin')).to.be.false;
+    expect(manager.hasModel('friend')).to.be.false;
+  }));
+
+  it('model', coroutine.wrap(function* () {
+    {
+      const ret = yield manager.model('Admin').insert({
+        name: '超级管理员',
+        email: 'admin@ucdok.com',
+        info: { role: 'admin' },
+        created_at: new Date(),
+      }).exec();
+      console.log(ret);
+      expect(ret.affectedRows).to.equal(1);
+      expect(ret.insertId).to.equal(1);
+    }
+    {
+      const ret = yield manager.model('Admin').getByPrimary({ id: 1 });
+      console.log(ret);
+      expect(ret).to.include({
+        id: 1,
+        name: '超级管理员',
+        email: 'admin@ucdok.com',
+      });
+    }
+    {
+      expect(function () {
+        manager.model('Haha');
+      }).to.throw('model "Haha" does not exists');
+    }
   }));
 
 });
