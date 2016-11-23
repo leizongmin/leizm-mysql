af90bb29821251b0d95fe5474896da2550fcbb7d
Zongmin Lei <leizongmin@gmail.com>
2016-11-23 21:46:53 +0800

    v0.0.6 schema支持 bool 类型

--- test/test_schema.js
+++ test/test_schema.js
@@ -12,7 +12,7 @@ const { createSchema } = require('../');
 
 describe('Schema', function () {
 
-  const query = createSchema({
+  const schema = createSchema({
     fields: {
       name: true,
       info: { type: 'json' },
@@ -24,11 +24,12 @@ describe('Schema', function () {
           return Buffer.from(v, 'base64').toString();
         },
       },
+      is_disabled: { type: 'bool' },
     },
   });
 
   it('serialize && unserialize', function () {
-    const data = query.serialize({
+    const data = schema.serialize({
       name: 'ABC',
       info: {
         url: 'http://baidu.com/',
@@ -39,7 +40,7 @@ describe('Schema', function () {
     console.log(data);
     expect(data).to.equal('{"name":"ABC","info":{"url":"http://baidu.com/","email":"me@ucdok.com"},"data":"Hello, world"}');
 
-    const ret = query.unserialize(data);
+    const ret = schema.unserialize(data);
     console.log(ret);
     expect(ret).to.deep.equal({
       name: 'ABC',
@@ -52,13 +53,13 @@ describe('Schema', function () {
   });
 
   it('formatInput & formatInputList', function () {
-    expect(query.formatInput({
+    expect(schema.formatInput({
       name: 'ABC',
       OK: true,
     })).to.deep.equal({
       name: 'ABC',
     });
-    expect(query.formatInput({
+    expect(schema.formatInput({
       name: 'ABC',
       OK: true,
       info: 'aaa',
@@ -66,7 +67,7 @@ describe('Schema', function () {
       name: 'ABC',
       info: '"aaa"',
     });
-    expect(query.formatInput({
+    expect(schema.formatInput({
       name: 'ABC',
       info: 'aaa',
       data: 'hahaha',
@@ -75,7 +76,7 @@ describe('Schema', function () {
       info: '"aaa"',
       data: 'aGFoYWhh',
     });
-    expect(query.formatInputList([{
+    expect(schema.formatInputList([{
       name: 'ABC',
       info: 'aaa',
       data: 'hahaha',
@@ -92,17 +93,32 @@ describe('Schema', function () {
       info: '"aaa"',
       data: 'aGFoYWhh',
     }]);
+    expect(schema.formatInput({
+      is_disabled: false,
+    })).to.deep.equal({
+      is_disabled: 0,
+    });
+    expect(schema.formatInput({
+      is_disabled: true,
+    })).to.deep.equal({
+      is_disabled: 1,
+    });
+    expect(schema.formatInput({
+      is_disabled: 1,
+    })).to.deep.equal({
+      is_disabled: 1,
+    });
   });
 
   it('formatOutput & formatOutputList', function () {
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       name: 'hello',
       data: 'aGFoYWhh',
     })).to.deep.equal({
       name: 'hello',
       data: 'hahaha',
     });
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       name: 'hello',
       data: 'aGFoYWhh',
       message: 'yes',
@@ -111,7 +127,7 @@ describe('Schema', function () {
       data: 'hahaha',
       message: 'yes',
     });
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       name: 'hello',
       data: 'aGFoYWhh',
       message: 'yes',
@@ -122,7 +138,7 @@ describe('Schema', function () {
       message: 'yes',
       info: 'aaa',
     });
-    expect(query.formatOutputList([{
+    expect(schema.formatOutputList([{
       name: 'hello',
       data: 'aGFoYWhh',
       message: 'yes',
@@ -141,29 +157,44 @@ describe('Schema', function () {
       message: 'yes',
       info: 'aaa',
     }]);
+    expect(schema.formatOutput({
+      is_disabled: 1,
+    })).to.deep.equal({
+      is_disabled: true,
+    });
+    expect(schema.formatOutput({
+      is_disabled: 0,
+    })).to.deep.equal({
+      is_disabled: false,
+    });
+    expect(schema.formatOutput({
+      is_disabled: null,
+    })).to.deep.equal({
+      is_disabled: false,
+    });
     // 畸形json字段
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       info: undefined,
     })).to.deep.equal({
       info: undefined,
     });
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       info: null,
     })).to.deep.equal({
       info: {},
     });
-    expect(query.formatOutput({
+    expect(schema.formatOutput({
       info: '',
     })).to.deep.equal({
       info: {},
     });
     expect(function () {
-      query.formatOutput({
+      schema.formatOutput({
         info: 123,
       });
     }).to.throw('jsonDecoder: invalid input type: 123');
     expect(function () {
-      query.formatOutput({
+      schema.formatOutput({
         info: '{"a":',
       });
     }).to.throw('jsonDecoder: fail to parse JSON');
