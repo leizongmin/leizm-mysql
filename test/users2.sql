b7223e5c14bb3574050ebc15707a09bde0d1015a
Zongmin Lei <leizongmin@gmail.com>
2016-12-15 10:43:41 +0800

    fix(test): SQL

--- test/users2.sql
+++ test/users2.sql
@@ -3,7 +3,7 @@ CREATE TABLE `users2` (
   `phone` varchar(50) NOT NULL DEFAULT '',
   `first_name` varchar(50) NOT NULL DEFAULT '',
   `last_name` varchar(255) NOT NULL DEFAULT '',
-  `info` text NOT NULL,
+  `info` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone` (`phone`),
   UNIQUE KEY `first_name` (`first_name`,`last_name`)
